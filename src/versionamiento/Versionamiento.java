2c0b1462c5bf27ca5391cba0b9eb8ee94c579191
asquinche <DELL@ANNY>
2014-06-05 22:10:53 -0500

    revision final

--- src/versionamiento/Versionamiento.java
+++ src/versionamiento/Versionamiento.java
@@ -37,20 +37,20 @@ public class Versionamiento {
                  String saludo="Hola";
                  try
                  {
-//Crear un objeto File se encarga de crear o abrir acceso a un archivo que se especifica en su constructor
+                 //Crear un objeto File se encarga de crear o abrir acceso a un archivo que se especifica en su constructor
                  File archivo=new File("texto.txt");
 
-//Crear objeto FileWriter que sera el que nos ayude a escribir sobre archivo
+                 //Crear objeto FileWriter que sera el que nos ayude a escribir sobre archivo
                  FileWriter escribir=new FileWriter(archivo,true);
 
-//Escribimos en el archivo con el metodo write 
+                 //Escribimos en el archivo con el metodo write 
                  escribir.write(saludo);
 
-//Cerramos la conexion
+                 //Cerramos la conexion
                  escribir.close();
 }
 
-//Si existe un problema al escribir cae aqui
+                 //Si existe un problema al escribir cae aqui
                 catch(IOException e){
                     System.out.println("Error al escribir");
 }
@@ -59,20 +59,20 @@ public class Versionamiento {
                  String texto="";
                  try
                  {
-//Creamos un archivo FileReader que obtiene lo que tenga el archivo
+                 //Creamos un archivo FileReader que obtiene lo que tenga el archivo
                    FileReader lector=new FileReader("texto.txt");
 
-//El contenido de lector se guarda en un BufferedReader
+                 //El contenido de lector se guarda en un BufferedReader
                    BufferedReader contenido=new BufferedReader(lector);
 
-//Con el siguiente ciclo extraemos todo el contenido del objeto "contenido" y lo mostramos
+                 //Con el siguiente ciclo extraemos todo el contenido del objeto "contenido" y lo mostramos
                    while((texto=contenido.readLine())!=null)
                    {
                        System.out.println(texto);
                    }
                  }
 
-//Si se causa un error al leer cae aqui
+                 //Si se causa un error al leer cae aqui
                  catch(IOException e)
                  {
                      System.out.println("Error al leer");
